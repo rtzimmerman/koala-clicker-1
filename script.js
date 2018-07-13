866ac4cf1900fca4198b67ba2f5c005b6b8e1cd3
Robert Zimmerman <zim2007@gmail.com>
2018-07-12 21:56:39 -0500

    Added an visual indicator and tab counter update

--- script.js
+++ script.js
@@ -19,4 +19,15 @@ function displayCount(event) {
 
 	// output click count to container in DOM
 	countContainer.innerHTML = clickCount + " clicks!";
+
+	// display count in browser tab
+	document.title = `${clickCount} Koala${clickCount > 1 ? 's' : ''} - Koala Clicker`
+
+	// create a visual indicator at 10 and 20 clicks
+	clickCount == 10 || clickCount == 20 ? rotateImage() : undefined;
+}
+
+function rotateImage(){
+	console.log('Wow!');
+	koalaImg.setAttribute('class', 'achievement-unlocked');
 }
